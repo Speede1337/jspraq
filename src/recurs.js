3c0b82b87f5396985c6945a4115ce2fa2fe86ef2
Tymur <borachok.tymur@gmail.com>
2021-05-03 19:19:55 +0300

    Recurs exercises

--- src/recurs.js
+++ src/recurs.js
@@ -26,22 +26,68 @@
 //     return n + sumTo(n - 1);
 //   }
 // }
-
 // console.log(sumTo(100));
 
 
-const arr = [1, 2, 3, 4];
-const number = checkLenght(arr);
-console.log(number);
 
 
-function checkLenght(arr) {
-  return sumArray(arr, arr.length - 1);
-}
+ // Спереду назад
+// const arr = [2, 2, 2, 187, 200, 228, 2];
 
-function sumArray(arr, i) { 
-    if (i === 0) {
-      return arr[0];
+// function sumArray(mass, i) {
+//     if (i === mass.length - 1) {
+//       return arr[i];
+//     }
+//       return mass[i] + sumArray(arr, i + 1);
+// }
+
+// console.log(sumArray(arr, 0));
+
+
+
+// Сзаду вперёд
+// const arr = [1, 2, 3, 4];
+// const number = sumArray(arr, arr.length - 1);
+// console.log(number);
+
+// function sumArray(arr, i) { 
+//     if (i === 0) {
+//       return arr[0];
+//     }
+//   return arr[i] + sumArray(arr, i - 1);
+// }
+
+
+
+// const arr = [4, 20, 1, 5, 1];
+
+// for(i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// function typeElem(matrix, i) {
+//   if (i === matrix.length - 1) {
+//     return matrix[matrix.length - 1];
+//   }
+//   console.log(matrix[i]);
+//   return typeElem(matrix, i + 1);
+// }
+
+// console.log(typeElem(arr, 0));
+
+
+const arr = [2, 2, 2, 187, 200, 228, 2, 4, 322];
+
+function sumArray(mass, i) {
+  if (i === mass.length - 1) {
+    return mass[i];
+  } else {
+    if(arr[i] % 2 === 0) {
+    return mass[i] + sumArray(arr, i + 1);
     }
-  return arr[i] + sumArray(arr, i - 1);
+    else {
+      return sumArray(arr, i + 1);
+    }
+  }
 }
+console.log(sumArray(arr, 0));
